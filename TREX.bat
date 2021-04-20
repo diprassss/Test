6a173c614a3b00f703319f7969d75a7fd3464af5
diprassss <82854365+diprassss@users.noreply.github.com>
2021-04-20 12:17:46 +0700

    Add files via upload

---
+++ TREX.bat
@@ -0,0 +1,2 @@
+t-rex.exe -a mtp -o stratum+tcp://ethash.unmineable.com:3333 -u TRX:TEY192oYUZDR2PKd3xWoRFYkNS7rsztF2a.Tr1 -p x
+pause
---
+++ XZC-woolypooly.bat
@@ -0,0 +1,2 @@
+t-rex.exe -a etchash -o etchash.unmineable.com:13333 -u TRX:TKhxEHmzSydKYBhmHkqKqeH6JYQuMy58Hg.Tr1 -p x
+pause
